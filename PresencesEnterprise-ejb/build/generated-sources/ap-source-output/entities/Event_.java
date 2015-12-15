8007febd007dce90ecc7487a2bfa87a9be36127a
tiagoestacio <2130380@my.ipleiria.pt>
2015-12-15 02:15:37 +0000

    antes de nova entidade attendantEvent

--- PresencesEnterprise-ejb/build/generated-sources/ap-source-output/entities/Event_.java
+++ PresencesEnterprise-ejb/build/generated-sources/ap-source-output/entities/Event_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-12T19:13:36")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-12T20:22:47")
 @StaticMetamodel(Event.class)
 public class Event_ { 
 
