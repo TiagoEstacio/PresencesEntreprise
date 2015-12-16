4b80db678373864e19c1aca5a43ebff8b632fd2d
tiagoestacio <2130380@my.ipleiria.pt>
2015-12-16 22:09:15 +0000

    Tentativa de push novamente

--- PresencesEnterprise-ejb/build/generated-sources/ap-source-output/entities/Attendant_.java
+++ PresencesEnterprise-ejb/build/generated-sources/ap-source-output/entities/Attendant_.java
@@ -7,11 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-16T21:45:49")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-16T21:15:58")
->>>>>>> a3754738cc8de3415777d26bbd4f2c7635f05897
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-16T22:05:24")
 @StaticMetamodel(Attendant.class)
 public class Attendant_ extends User_ {
 
--- PresencesEnterprise-ejb/build/generated-sources/ap-source-output/entities/Manager_.java
+++ PresencesEnterprise-ejb/build/generated-sources/ap-source-output/entities/Manager_.java
@@ -5,11 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-16T21:45:49")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-16T21:15:58")
->>>>>>> a3754738cc8de3415777d26bbd4f2c7635f05897
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-16T22:05:24")
 @StaticMetamodel(Manager.class)
 public class Manager_ extends User_ {
 
