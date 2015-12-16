bb7eb71138e1b0f0889fc5d5a64a8d0a566e0438
tiagoestacio <2130380@my.ipleiria.pt>
2015-12-16 21:40:45 +0000

    Merge branch 'master' of https://github.com/TiagoEstacio/PresencesEntreprise
    # Conflicts:
    #	PresencesEnterprise-ejb/build/classes/entities/Administrator_.class
    #	PresencesEnterprise-ejb/build/classes/entities/AttendantCategory_.class
    #	PresencesEnterprise-ejb/build/classes/entities/Attendant_.class
    #	PresencesEnterprise-ejb/build/classes/entities/Category_.class
    #	PresencesEnterprise-ejb/build/classes/entities/EventCategory_.class
    #	PresencesEnterprise-ejb/build/classes/entities/Event_.class
    #	PresencesEnterprise-ejb/build/classes/entities/Manager_.class
    #	PresencesEnterprise-ejb/build/classes/entities/UserGroup_.class
    #	PresencesEnterprise-ejb/build/classes/entities/User_.class
    #	PresencesEnterprise-ejb/dist/PresencesEnterprise-ejb.jar
    #	PresencesEnterprise-war/dist/PresencesEnterprise-war.war

--- PresencesEnterprise-ejb/build/generated-sources/ap-source-output/entities/Attendant_.java
+++ PresencesEnterprise-ejb/build/generated-sources/ap-source-output/entities/Attendant_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-16T19:49:40")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-16T21:31:13")
 @StaticMetamodel(Attendant.class)
 public class Attendant_ extends User_ {
 
--- PresencesEnterprise-ejb/build/generated-sources/ap-source-output/entities/Event_.java
+++ PresencesEnterprise-ejb/build/generated-sources/ap-source-output/entities/Event_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-16T19:49:40")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-16T21:31:13")
 @StaticMetamodel(Event.class)
 public class Event_ { 
 
